191ddc7893ebed44a7b9d23f604d52b9c03792fc
Abhishek <abhishekshettyy18@gmail.com>
2026-08-13 11:31:07 +0530

    Add python learning variables

---
+++ typeconversion.py
@@ -0,0 +1,9 @@
+age = 21 
+weight = 64.0
+is_student = False
+name = "cj"
+print(type(age))
+s = "100"
+print(int(s)+age)
+
+
---
+++ variables.py
@@ -0,0 +1,6 @@
+a = 10
+b = 20
+a=b=c=10
+a,b,c=10,20,30
+print(c)
+print(b)
