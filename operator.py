0685602c64cc7c851c17426b199a9f5e3a7393a7
Abhishek <abhishekshettyy18@gmail.com>
2026-08-15 18:06:16 +0530

    Add string opearaor practice

---
+++ operator.py
@@ -0,0 +1,16 @@
+#assignment operator
+a = 10
+a += 100
+print(a)
+
+#comparission operator
+c = 10
+d = 20
+print(c==d)
+print(c!=d)
+print(c<d)
+print(c>d)
+
+#logical operator
+print(True and False)
+print(True or False)
