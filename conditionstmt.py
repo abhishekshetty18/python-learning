e6577a754ec2efe8bc002d94b911fca928f78893
Abhishek <abhishekshettyy18@gmail.com>
2026-08-20 15:28:00 +0530

    Add string conditionstmt practice

---
+++ conditionstmt.py
@@ -0,0 +1,13 @@
+time = 20
+
+if time==8:
+    print("It is a breakfast time")
+
+elif time==13:
+    print("It is a lunch time")
+
+elif time==20:
+    print("It is a dinner time")
+
+else:
+    print("It is not a meal time")
